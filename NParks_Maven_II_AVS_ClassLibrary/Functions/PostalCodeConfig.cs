9ad62c8d8babed4f98f8a283ea779a6b6261c2ee
Exrion - Titus Lim <titus.happy@gmail.com>
2022-10-05 10:55:24 +0800

    Example api project for D365

---
+++ NParks_Maven_II_AVS_ClassLibrary/Functions/PostalCodeConfig.cs
@@ -0,0 +1,32 @@
+﻿using NParks_Maven_II_AVS_WebApi.Services;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace NParks_Maven_II_AVS_ClassLibrary.Functions
+{
+    public class PostalCodeConfig
+    {
+        private readonly HttpClient _client = HTTPClient.getClient();
+
+        public string getPostalCodeConfigs(string apiParams)
+        {
+            HttpResponseMessage res = _client.GetAsync(apiParams).Result;
+
+            if (res.IsSuccessStatusCode)
+            {
+                var data = res.Content.ReadAsStream().ToString();
+                return data!;
+            }
+            else
+            {
+                return null!;
+            }
+        }
+    }
+}
---
+++ NParks_Maven_II_AVS_ClassLibrary/Services/HTTPClient.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http.Headers;
+
+namespace NParks_Maven_II_AVS_WebApi.Services
+{
+    public class HTTPClient
+    {
+        //Not working
+        private static readonly string? RESOURCE = System.Environment.GetEnvironmentVariable("API_RESOURCE");
+        private static readonly string? BASE = System.Environment.GetEnvironmentVariable("API_BASE");
+
+        public static HttpClient getClient()
+        {
+            return prepareClient();
+        }
+
+        private static HttpClient prepareClient()
+        {
+            //New Client
+            HttpClient client = new HttpClient
+            {
+                //BaseAddress = new Uri($"{RESOURCE}${BASE}"),
+                BaseAddress = new Uri("https://org7f3624ff.api.crm5.dynamics.com/api/data/v9.2/"),
+                Timeout = new TimeSpan(0, 2, 0)
+            };
+
+            //Default Headers
+            HttpRequestHeaders headers = client.DefaultRequestHeaders;
+            headers.Add("OData-MaxVersion", "4.0");
+            headers.Add("OData-Version", "4.0");
+            headers.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            return client;
+        }
+    }
+}
